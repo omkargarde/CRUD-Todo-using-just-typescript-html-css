33ab792eba658e258c62a2188cb55a6ad247a105
omkargarde <42486951+omkargarde@users.noreply.github.com>
2023-10-10 21:07:41 +0530

    updated project name

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,58 @@
 import { v4 as uuidV4 } from "uuid";
 
-type Task = { id: string, title: string, completed: boolean, createdAt: Date }
+type Task = { id: string; title: string; completed: boolean; createdAt: Date };
 const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.querySelector<HTMLFormElement>("#new-task-form");
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
 const tasks: Task[] = loadTasks();
-tasks.forEach(addListItem)
-form?.addEventListener("submit", e => {
-    e.preventDefault();
-    if (input?.value == "" || input?.value == null) return;
-    const newTask: Task = {
-        id: uuidV4(),
-        title: input.value,
-        completed: false,
-        createdAt: new Date()
-    }
-    tasks.push(newTask);
-    addListItem(newTask);
-    input.value = "";
-})
+tasks.forEach(addListItem);
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input?.value == "" || input?.value == null) return;
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  tasks.push(newTask);
+  addListItem(newTask);
+  input.value = "";
+});
 
 function addListItem(task: Task) {
-    const item = document.createElement("li");
-    const label = document.createElement("label");
-    const checkbox = document.createElement("input");
-    const deleteButton = document.createElement("button");
-    deleteButton.id = task.id;
-    deleteButton.innerText = "delete";
-    deleteButton.onclick = removeTask
-    checkbox.addEventListener("change", () => {
-        task.completed = checkbox.checked;
-        saveTask();
-    })
-    checkbox.type = "checkbox";
-    checkbox.checked = task.completed;
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  const deleteButton = document.createElement("button");
+  deleteButton.id = task.id;
+  deleteButton.innerText = "delete";
+  deleteButton.onclick = removeTask;
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    saveTask();
+  });
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
 
-    label.append(checkbox, task.title, " ", deleteButton);
-    item.append(label);
-    list?.append(item);
+  label.append(checkbox, task.title, " ", deleteButton);
+  item.append(label);
+  list?.append(item);
 }
 
 function saveTask() {
-    localStorage.setItem("TASKS", JSON.stringify(tasks));
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
 }
 function loadTasks(): Task[] {
-    const taskJson = localStorage.getItem("TASKS")
-    if (taskJson == null) return [];
-    return JSON.parse(taskJson);
+  const taskJson = localStorage.getItem("TASKS");
+  if (taskJson == null) return [];
+  return JSON.parse(taskJson);
 }
 
 function removeTask(e: Event) {
-    const target = e.target as HTMLButtonElement;
-    document.getElementById(target.id)?.parentElement?.remove();
-    const deletedtask = tasks.filter((task) => task.id !== target.id);
-    localStorage.setItem("TASKS", JSON.stringify(deletedtask));
+  const target = e.target as HTMLButtonElement;
+  document.getElementById(target.id)?.parentElement?.remove();
+  const deleteTask = tasks.filter((task) => task.id !== target.id);
+  localStorage.setItem("TASKS", JSON.stringify(deleteTask));
 }
+
